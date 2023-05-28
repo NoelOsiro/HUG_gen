d9192060e62a40f9820fb023f98eaeaebe414ba8
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-05-28 01:44:04 +0000

    Code clean up

--- src/Components/Featured/Features.tsx
+++ src/Components/Featured/Features.tsx
@@ -1,33 +1,62 @@
 import React from 'react';
 
-const Features = ()=> {
+interface Ifeats {
+  icon: string,
+  title: string,
+  description:string
+  isBackground?:boolean
+}
+const Features = () => {
+  const featureBoxes: Ifeats[] = [
+    {
+      icon: 'bi bi-award',
+      title: 'Lorem Ipsum Delino',
+      description: 'Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident',
+    },
+    {
+      icon: 'bi bi-card-checklist',
+      title: 'Dolor Sitema',
+      description: 'Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata',
+      isBackground: true,
+    },
+    {
+      icon: 'bi bi-binoculars',
+      title: 'Sed ut perspiciatis',
+      description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+    },
+  ];
+
   return (
     <section id="featured-services">
       <div className="container">
         <div className="row">
-
-          <div className="col-lg-4 box">
-            <i className="bi bi-briefcase"></i>
-            <h4 className="title"><a href="">Lorem Ipsum Delino</a></h4>
-            <p className="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-          </div>
-
-          <div className="col-lg-4 box box-bg">
-            <i className="bi bi-card-checklist"></i>
-            <h4 className="title"><a href="">Dolor Sitema</a></h4>
-            <p className="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
-          </div>
-
-          <div className="col-lg-4 box">
-            <i className="bi bi-binoculars"></i>
-            <h4 className="title"><a href="">Sed ut perspiciatis</a></h4>
-            <p className="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-          </div>
-
+          {featureBoxes.map((box, index) => (
+            <FeatureBox
+              key={index}
+              icon={box.icon}
+              title={box.title}
+              description={box.description}
+              isBackground={box.isBackground}
+            />
+          ))}
         </div>
       </div>
     </section>
   );
-}
+};
+
+const FeatureBox = (props:Ifeats) => {
+  const boxClassName = `col-lg-4 box ${props.isBackground ? 'box-bg' : ''}`;
+
+  return (
+    <div className={boxClassName}>
+      <i className={props.icon}></i>
+      <h4 className="title">
+        <a href="">{props.title}</a>
+      </h4>
+      <p className="description">{props.description}</p>
+    </div>
+  );
+};
 
 export default Features;
