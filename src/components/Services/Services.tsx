9afc1bb8d56b115b981e7b623b6d8a399ec7592f
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-07-07 18:44:18 +0000

    Servie

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -50,19 +50,19 @@ const Service = () => {
           title={"What We Do?"}
           text={"We believe that we can save more lives with you"}
         />
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 p-4">
           {servicesData.map((service, index) => (
             <div
               key={index}
-              className="relative w-full flex justify-between items-center mb-45px transition duration-300 bg-white shadow-md xl:p-6 lg:p-4 rounded-lg"
+              className="relative w-full flex justify-between items-center mb-45px transition duration-300 bg-white shadow-md p-8 xl:p-6 lg:p-4 rounded-lg"
             >
               <div className="relative w-[60px] text-blue-600 pr-8">
-                <div className=" text-blue-600 text-3xl md:text-4xl lg:text-4xl xl:text-4xl leading-60px mt-5">
+                <div className=" text-blue-600 text-3xl md:text-4xl leading-60px mt-5">
                   {service.icon}
                 </div>
               </div>
               <div className="relative pl-8">
-                <h3 className="font-bold mb-2">{service.title}</h3>
+                <h3 className="font-bold mb-2 text-xl md:text-2xl">{service.title}</h3>
                 <p className="">{service.text}</p>
                 <div className="absolute left-0 top-0 h-full border-l border-yellow-300"></div>
                 <div className="absolute left-0 top-1/2 h-8 w-1 bg-blue-500 transform -translate-y-1/2"></div>
